d5dd44ac72a913526d25e7147231159fdb6e9d44
Chris Carrington <chris.carrington@glew.io>
2023-02-11 03:03:55 -0800

    little more on action

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,11 +1,14 @@
+import validate from '$lib/form/validate'
+import actionCatch from '$lib/util/actionCatch'
 import type { Actions } from './$types'
+import schema from '$lib/schema/newsletterSignUp'
 // import newsletterSignUp from '$lib/actions/newsletterSignUp'
 
 
 export const actions = {
   async newsletterSignUp ({ request }) {
-    // try {
-    //   const { fields, error } = await validate(request, schema)
+    try {
+      const { fields, error } = await validate(request, schema)
 
     //   if (error) throw error
     //   else {
@@ -14,8 +17,8 @@ export const actions = {
     //     if (!user) await createUser({ ...fields, isNewsletterSubscriber: 'on' })
     //     else if (!user.isNewsletterSubscriber) await updateUser({ email: fields.email.toString() }, { isNewsletterSubscriber: true })
     //   }
-    // } catch (e) {
-    //   return actionCatch(e)
-    // }
+    } catch (e) {
+      return actionCatch(e)
+    }
   }
 } satisfies Actions
