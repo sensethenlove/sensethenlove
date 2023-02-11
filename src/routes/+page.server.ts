930bcee93bea6d2873abfed11f65a762c2dbf300
Chris Carrington <chris.carrington@glew.io>
2023-02-11 03:06:43 -0800

    introducing prisma to see if that breaks home

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,6 +2,7 @@ import validate from '$lib/form/validate'
 import actionCatch from '$lib/util/actionCatch'
 import type { Actions } from './$types'
 import schema from '$lib/schema/newsletterSignUp'
+import findFirstUser from '$lib/prisma/findFirstUser'
 // import newsletterSignUp from '$lib/actions/newsletterSignUp'
 
 
@@ -10,13 +11,13 @@ export const actions = {
     try {
       const { fields, error } = await validate(request, schema)
 
-    //   if (error) throw error
-    //   else {
-    //     const user = await findFirstUser({ email: fields.email.toString() })
+      if (error) throw error
+      else {
+        const user = await findFirstUser({ email: fields.email.toString() })
 
     //     if (!user) await createUser({ ...fields, isNewsletterSubscriber: 'on' })
     //     else if (!user.isNewsletterSubscriber) await updateUser({ email: fields.email.toString() }, { isNewsletterSubscriber: true })
-    //   }
+      }
     } catch (e) {
       return actionCatch(e)
     }
