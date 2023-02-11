b7fd0d5cfb9529dc79ad354e0ac85a84c9808c5f
Chris Carrington <chris.carrington@glew.io>
2023-02-11 03:01:12 -0800

    bareminimum action

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,21 @@
 import type { Actions } from './$types'
-import newsletterSignUp from '$lib/actions/newsletterSignUp'
+// import newsletterSignUp from '$lib/actions/newsletterSignUp'
 
 
 export const actions = {
-  newsletterSignUp
+  async newsletterSignUp ({ request }) {
+    // try {
+    //   const { fields, error } = await validate(request, schema)
+
+    //   if (error) throw error
+    //   else {
+    //     const user = await findFirstUser({ email: fields.email.toString() })
+
+    //     if (!user) await createUser({ ...fields, isNewsletterSubscriber: 'on' })
+    //     else if (!user.isNewsletterSubscriber) await updateUser({ email: fields.email.toString() }, { isNewsletterSubscriber: true })
+    //   }
+    // } catch (e) {
+    //   return actionCatch(e)
+    // }
+  }
 } satisfies Actions
