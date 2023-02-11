73829a65495bbcbdd3653511aea94a9b1199094a
Chris Carrington <chris.carrington@glew.io>
2023-02-11 04:13:30 -0800

    dynamic prisma client import

--- src/lib/prisma/prisma.ts
+++ src/lib/prisma/prisma.ts
@@ -1,12 +1,13 @@
-import { PrismaClient } from '@prisma/client/edge'
 import { PUBLIC_ENVIRONMENT } from '$env/static/public'
 
 
 // Typically module code is run once on the server but w/ HMR in development module code is run multiple times so in develpment add prisma to global so new PrismaClient() which creates a connection pool on each call is not called multiple times
 // Prisma Recommendation: "Your application should generally only create one instance of PrismaClient": https://www.prisma.io/docs/concepts/components/prisma-client/working-with-prismaclient/instantiate-prisma-client
 // Prisma Suggestion: https://www.prisma.io/docs/guides/database/troubleshooting-orm/help-articles/nextjs-prisma-client-dev-practices
-if (PUBLIC_ENVIRONMENT === 'development' && !global.prisma) global.prisma = new PrismaClient()
+async function getClient () {
+  const client = PUBLIC_ENVIRONMENT === 'development' ? await import('@prisma/client') : await import('@prisma/client/edge')
+  if (PUBLIC_ENVIRONMENT === 'development' && !global.prisma) global.prisma = new client.PrismaClient()
+  return PUBLIC_ENVIRONMENT === 'development' ? global.prisma : new client.PrismaClient()
+}
 
-export default PUBLIC_ENVIRONMENT === 'development' ?
-  global.prisma :
-  new PrismaClient()
+export default getClient()
