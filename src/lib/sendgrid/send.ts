d4253920831b678bb1696ffb936c07f964b25161
Chris Carrington <chris.carrington@glew.io>
2023-02-15 10:00:29 -0800

    remove sendgrid package from project b/c it does not work on cloudflare workers

--- src/lib/sendgrid/send.ts
+++ src/lib/sendgrid/send.ts
@@ -1,8 +1,18 @@
-import sendgrid from '@sendgrid/mail'
 import { SENDGRID_API_KEY } from '$env/static/private'
 
 
 export default async ({ to, subject, html }: { to: string, subject: string, html: string }) => {
-  sendgrid.setApiKey(SENDGRID_API_KEY)
-  return await sendgrid.send({ from: 'us@sensethenlove.com', to, subject, html })
+  return await fetch('https://api.sendgrid.com/v3/mail/send', {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${ SENDGRID_API_KEY }`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      subject,
+      content: [ { type: 'text/html', value: html } ],
+      from: { email: 'us@sensethenlove.com' },
+      personalizations: [ { to: [ { email: to } ] } ]
+    })
+  })
 }
