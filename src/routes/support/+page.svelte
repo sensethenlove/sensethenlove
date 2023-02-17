6e5d1dc016d31eba9ab680f82edd535fcb390e90
Chris Carrington <chris.carrington@glew.io>
2023-02-17 01:37:01 -0800

    support coming soon

---
+++ src/routes/support/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import Head from '$lib/components/Head.svelte'
+  import Title from '$lib/components/Title.svelte'
+</script>
+
+
+<Head title="Support" description="Thank you for supporting Sense Then Love!" />
+<Title h1="Support" />
+<Title h3="Coming soon!" />
