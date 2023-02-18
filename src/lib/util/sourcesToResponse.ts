ee180b69450bb7ca95a2403c33c6d90d60464742
Chris Carrington <chris.carrington@glew.io>
2023-02-18 01:24:26 -0800

    fix sources to response bug where all categories were showing up in source even for quotes that were not displayed

--- src/lib/util/sourcesToResponse.ts
+++ src/lib/util/sourcesToResponse.ts
@@ -1,17 +1,17 @@
-import type { Source } from '$lib/util/types'
+import type { Source, Quote, Author, QuoteCategory } from '$lib/util/types'
 
 
 export default function (sources: Source[], urlParamAuthor: string | null, urlParamCategory?: string) {
   let responseAuthor // the author we will place in the response
   let responseCategory // the category we will place in the response
-  const authors = new Map() // use map so duplicates are removed
-  const categories = new Map() // use map so duplicates are removed
+  const authors: Map<string, Author> = new Map() // use map so duplicates are removed
+  const categories: Map<string, QuoteCategory> = new Map() // use map so duplicates are removed
   const SOURCES_LENGTH_MINUS_ONE = sources.length - 1 // indexes (sourceIndex) in for loop below go to 1 minus the length
 
   for (let sourceIndex = SOURCES_LENGTH_MINUS_ONE; sourceIndex >= 0; sourceIndex--) { // loop sources backwards b/c we may remove some sources
     let source = sources[sourceIndex]
     let urlAuthorWroteSource = false
-    let sourceCategories = new Map() // use map so duplicates are removed
+    let sourceCategories: Map<string, QuoteCategory> = new Map() // use map so duplicates are removed
 
     for (const author of source.authors) {
       authors.set(author.id, author) // place each author in map
@@ -30,11 +30,12 @@ export default function (sources: Source[], urlParamAuthor: string | null, urlPa
 
       for (const category of quote.categories) {
         categories.set(category.id, category) // place each category in map of all categories
-        sourceCategories.set(category.id, category) // place each category in map of source categories
 
-        if (urlParamCategory && category.slug === urlParamCategory) { // if a category param is requested in the URL AND the category in the loop matches the urlParamCategory
-          responseCategory = category
-          quoteHasUrlCategory = true
+        if (!urlParamCategory) addQuoteCategoriesToSourceCategories(quote, sourceCategories) // if no category is requested in the url => place categories from quote into source categories
+        else if (category.slug === urlParamCategory) { // if a category param is requested in the URL AND the category in the loop matches the urlParamCategory
+          responseCategory = category // save this full varialbe representing the url param
+          quoteHasUrlCategory = true // tip flag indicating quote from url found
+          addQuoteCategoriesToSourceCategories(quote, sourceCategories) // place categories from quote into source categories
         }
       }
 
@@ -53,5 +54,12 @@ export default function (sources: Source[], urlParamAuthor: string | null, urlPa
     category: responseCategory,
     authors: [...authors.values()].sort((a, b) => Number(a.name > b.name) - Number(a.name < b.name)), // sort authors by name
     categories: [...categories.values()].sort((a, b) => Number(a.name > b.name) - Number(a.name < b.name)) // sort categories by name
+  }
+}
+
+
+function addQuoteCategoriesToSourceCategories (quote: Quote, sourceCategories: Map<string, QuoteCategory>) {
+  for (const category of quote.categories) {
+    sourceCategories.set(category.id, category)
   }
 }
