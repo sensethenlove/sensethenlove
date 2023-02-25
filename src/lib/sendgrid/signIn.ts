876e7b5bfc46230ef0a0ffff1fea6eff72087eac
Chris Carrington <chris.carrington@glew.io>
2023-02-24 21:57:07 -0800

    update packages + attem;t sending for real sign in email

--- src/lib/sendgrid/signIn.ts
+++ src/lib/sendgrid/signIn.ts
@@ -24,8 +24,20 @@ export default async (token: string, email: string, firstName: string | null) =>
       },
       "subject": "Test Subject",
       "content": [{
-        "type": "text/plain",
-        "value": "Test message content",
+        "type": "text/html",
+        "value": `
+          <table style="padding: 18px 0 27px 0; color: #273142; width: 100%; font-size: 16px; text-align:center; background-color:#f9f5f2; font-family: Inter, ui-sans-serif, system-ui, -apple-system, Segoe UI, Roboto, Helvetica Neue, Arial, Noto Sans, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji;">
+            <tr>
+              <td>
+                <img style="margin-bottom: 6px; max-width: 63px;" src="https://sensethenlove.com/cdn-cgi/imagedelivery/awgX85h4ifgiJaXRhZTMNw/79da5cb2-c86d-4a96-316e-7c975bfc1500/public" />
+                <div style="color: #273142; font-size:27px; margin-bottom: 9px; font-family: papyrus, Inter, ui-sans-serif, system-ui, -apple-system, Segoe UI, Roboto, Helvetica Neue, Arial, Noto Sans, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji;">Welcome${ firstName ? ' ' + firstName : '' }!</div>
+                <div style="margin-bottom: 9px;"><a href="https://sensethenlove.com/social/sign-in/verify?token=${ token }" target="_blank">Please click this link to sign in to Sense Then Love!</a></div>
+                <div style="color: #273142; margin-bottom: 9px;">Link valid for <strong>9 minutes</strong></div>
+                <div style="color: #273142;">Link must be clicked from the <strong>same IP address</strong> that filled out the sign in form</div>
+              </td>
+            </tr>
+          </table>
+        `,
       }],
     }),
   }))
