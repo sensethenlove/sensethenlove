6e6a37c97d6b8322d341cb2fa77356d24a8935c4
Chris Carrington <chris.carrington@glew.io>
2023-03-09 10:08:16 -0800

    2 id types for youtube

--- src/lib/youtube/getId.ts
+++ src/lib/youtube/getId.ts
@@ -1,6 +1,7 @@
 export default (url: string) => {
-  const split = url.split('=')
+  const splitKey = url.includes('be/') ? 'be/' : '='
+  const split =  url.split(splitKey)
 
-  if (split.length !== 2) throw new Error('Please provide a valid url, eg: https://www.youtube.com/watch?v=3G4kCi_ldr8')
+  if (split.length !== 2 || split[1].length !== 11) throw new Error('Please provide a valid url, eg: https://www.youtube.com/watch?v=3G4kCi_ldr8 OR https://youtu.be/3G4kCi_ldr8')
   else return split[1]
 }
